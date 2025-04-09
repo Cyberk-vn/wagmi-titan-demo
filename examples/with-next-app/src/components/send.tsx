91099d3fa9485ae303315f823080872c03844da8
phongvv <phongvuvan@cyberk.io>
2025-04-09 18:41:54 +0700

    Add sign message example

--- examples/with-next-app/src/components/send.tsx
+++ examples/with-next-app/src/components/send.tsx
@@ -1,18 +1,30 @@
 "use client";
 
 import { parseEther } from "viem";
-import { useAccount, useSendTransaction } from "wagmi";
+import { useAccount, useSendTransaction, useSignMessage } from "wagmi";
 import { titanTestnet } from "../titan-testnet";
+import { useVerifyMessage } from "wagmi";
+import { useState } from "react";
 
 export function Send() {
-  const { address } = useAccount();
   const { sendTransaction } = useSendTransaction();
+  const { signMessageAsync } = useSignMessage();
+
+  const account = useAccount();
+
+  const [signature, setSignature] = useState("");
+
+  const { data } = useVerifyMessage({
+    message: "Hello, world!",
+    signature: signature as `0x${string}`,
+    address: "0x4e2163B8F6F6e29CBFe7f5fD469aC386258f79a4",
+  });
 
   const handleSendTransaction = async () => {
     try {
       const tx = await sendTransaction({
-        to: "0x0000000000000000000000000000000000000000",
-        value: parseEther("0.001"),
+        to: "0x4e2163B8F6F6e29CBFe7f5fD469aC386258f79a4",
+        value: parseEther("0.1"),
         chainId: titanTestnet.id,
       });
       console.log("Transaction sent:", tx);
@@ -21,5 +33,23 @@ export function Send() {
     }
   };
 
-  return <button onClick={handleSendTransaction}>Send</button>;
+  const handleSignMessage = async () => {
+    try {
+      const message = "Hello, world!";
+      const signature = await signMessageAsync({ message });
+      console.log("Message signed:", signature);
+      setSignature(signature);
+    } catch (error) {
+      console.error("Message signing failed:", error);
+    }
+  };
+
+  console.log("=======isVerified", data);
+  console.log("=======account", account);
+  return (
+    <>
+      <button onClick={handleSendTransaction}>Send</button>
+      <button onClick={handleSignMessage}>Sign Message</button>
+    </>
+  );
 }
